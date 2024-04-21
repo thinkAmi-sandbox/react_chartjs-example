788274b77c7811659ca31cf7536ffbbc5f0ba055
thinkAmi <dev.thinkami+git@gmail.com>
2024-04-21 17:50:28 +0900

    add: pie chart with always tooltip

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -19,6 +19,9 @@ import { Route as rootRoute } from './routes/__root'
 const PiechartPiechartwithlegendontherightLazyImport = createFileRoute(
   '/pie_chart/pie_chart_with_legend_on_the_right',
 )()
+const PiechartPiechartwithalwaystooltipLazyImport = createFileRoute(
+  '/pie_chart/pie_chart_with_always_tooltip',
+)()
 const PiechartPiechartwithalwaystextLazyImport = createFileRoute(
   '/pie_chart/pie_chart_with_always_text',
 )()
@@ -34,6 +37,16 @@ const PiechartPiechartwithlegendontherightLazyRoute =
     getParentRoute: () => rootRoute,
   } as any).lazy(() =>
     import('./routes/pie_chart/pie_chart_with_legend_on_the_right.lazy').then(
+      (d) => d.Route,
+    ),
+  )
+
+const PiechartPiechartwithalwaystooltipLazyRoute =
+  PiechartPiechartwithalwaystooltipLazyImport.update({
+    path: '/pie_chart/pie_chart_with_always_tooltip',
+    getParentRoute: () => rootRoute,
+  } as any).lazy(() =>
+    import('./routes/pie_chart/pie_chart_with_always_tooltip.lazy').then(
       (d) => d.Route,
     ),
   )
@@ -67,6 +80,10 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof PiechartPiechartwithalwaystextLazyImport
       parentRoute: typeof rootRoute
     }
+    '/pie_chart/pie_chart_with_always_tooltip': {
+      preLoaderRoute: typeof PiechartPiechartwithalwaystooltipLazyImport
+      parentRoute: typeof rootRoute
+    }
     '/pie_chart/pie_chart_with_legend_on_the_right': {
       preLoaderRoute: typeof PiechartPiechartwithlegendontherightLazyImport
       parentRoute: typeof rootRoute
@@ -79,6 +96,7 @@ declare module '@tanstack/react-router' {
 export const routeTree = rootRoute.addChildren([
   PiechartFirstpiechartLazyRoute,
   PiechartPiechartwithalwaystextLazyRoute,
+  PiechartPiechartwithalwaystooltipLazyRoute,
   PiechartPiechartwithlegendontherightLazyRoute,
 ])
 
